bbf58fcd02059f710c6abec0903772a9e74d9641
Michalis Pardalos <m.pardalos@gmail.com>
2018-10-19 13:54:17 +0100

    Testing function in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "decoder.hpp"
 
 int main() {
-    std::cout << "This will be an awesome MIPS Simulator...\nEventually\n";
+    std::cout << "Testing: \n\n";
+    unsigned int i = 0b00001010000000000000000000000000;
+    decode(i);
 }
