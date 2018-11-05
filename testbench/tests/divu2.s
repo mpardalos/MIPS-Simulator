2a82ce3ab619d074bbb8134d6a0a38ead24f0cbe
Michalis Pardalos <m.pardalos@gmail.com>
2018-11-05 17:07:28 +0000

    Use hardcoded instruction for divu2 test
    Assembler emits zero-division checking code

--- testbench/tests/divu2.s
+++ testbench/tests/divu2.s
@@ -1,5 +1,8 @@
+# emit the division instruction directly 
+# because gas also emits division by zero checking code
+.set noreorder
 .text
     addi $t1, $t1, -98 
     addi $t2, $t2, 4
-    divu $t1, $t1, $t2 
+    .word 0x12a001b # t1 / t2
     jr $0
