778b3640bd64c9deef62036583c8661bb97e636f
Michalis Pardalos <m.pardalos@gmail.com>
2018-11-08 19:53:24 +0000

    Add an echo test program

---
+++ testbench/tests/echo.s
@@ -0,0 +1,14 @@
+.text
+        li $t5, 0x30000000 # Address of getc
+        li $t4, 126 # 126 = ~, which terminates
+        li $t3, 10 # newline
+
+.read:  lw $t1, 0($t5)
+        beq $t1, $t4, .exit
+        nop
+
+        sw $t1, 4($t5)
+        j .read
+
+.exit:  jr $0
+
