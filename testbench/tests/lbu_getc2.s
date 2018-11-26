151cc45e5b24f0f45be89168198f103178be9061
Michalis Pardalos <m.pardalos@gmail.com>
2018-11-26 13:52:31 +0000

    Test getc with lbu and lhu

---
+++ testbench/tests/lbu_getc2.s
@@ -0,0 +1,17 @@
+.text
+    li $t0, 0x30000000
+
+    lbu $t1, 3($t0)
+
+    li $t2, 0xFF
+
+    beq $t1, $t2, ok
+    nop
+
+nok: 
+    li $v0, 1
+    jr $0
+
+ok: 
+    li $v0, 10
+    jr $0
---
+++ testbench/tests/lhu_getc2.s
@@ -0,0 +1,17 @@
+.text
+    li $t0, 0x30000000
+
+    lhu $t1, 2($t0)
+
+    ori $t2, 0xFFFF
+
+    beq $t1, $t2, ok
+    nop
+
+nok: 
+    li $v0, 1
+    jr $0
+
+ok: 
+    li $v0, 10
+    jr $0
