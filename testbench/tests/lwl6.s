1cb8faabc6c97cee22ba52af7f12a4978d4ad2bc
Michalis Pardalos <m.pardalos@gmail.com>
2018-11-27 19:12:13 +0000

    Fix assembler fuckup in lwl6 test

--- testbench/tests/lwl6.s
+++ testbench/tests/lwl6.s
@@ -1,9 +1,12 @@
+.set noat
 .text
-    li $t2, 0x23FF0000
+    li $t2, 0x23FFFFFC
     li $t1, 0x12345678
-    sw $t1, 0xFFFC($t2)
+
+    sw $t1, ($t2)
 
     li $v0, 0x87654321
-    lwl $v0, 65532($t2)
+
+    lwl $v0, ($t2)
 
     jr  $0
